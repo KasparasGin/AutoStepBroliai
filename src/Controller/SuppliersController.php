7078f2eac2590f4832f46600589ea94b104fa188
tomasjur <tomasjkaunas@gmail.com>
2018-12-03 14:46:20 +0200

    Tiekeju meniu

--- src/Controller/SuppliersController.php
+++ src/Controller/SuppliersController.php
@@ -8,11 +8,51 @@ use Symfony\Component\Routing\Annotation\Route;
 class SuppliersController extends AbstractController
 {
     /**
-     * @Route("/suppliers", name="suppliersMenu")
+     * @Route("/suppliers/index", name="suppliers")
      */
     public function showSuppliersMenu()
     {
         return $this->render('suppliers/index.html.twig', [
+            'controller_name' => 'SuppliersController',
+        ]);
+    }
+
+    /**
+     * @Route("/suppliers/addSupplier", name="addSupplier")
+     */
+    public function addSupplier()
+    {
+        return $this->render('suppliers/addSupplier.html.twig', [
+            'controller_name' => 'SuppliersController',
+        ]);
+    }
+
+    /**
+     * @Route("/suppliers/editSupplier", name="editSupplier")
+     */
+    public function editSupplier()
+    {
+        return $this->render('suppliers/editSupplier.html.twig', [
+            'controller_name' => 'SuppliersController',
+        ]);
+    }
+
+    /**
+     * @Route("/suppliers/deleteSupplier", name="deleteSupplier")
+     */
+    public function deleteSupplier()
+    {
+        return $this->render('suppliers/deleteSupplier.html.twig', [
+            'controller_name' => 'SuppliersController',
+        ]);
+    }
+
+    /**
+     * @Route("/suppliers/paymentsForSuppliers", name="paymentsForSuppliers")
+     */
+    public function paymentsForSuppliers()
+    {
+        return $this->render('suppliers/paymentsForSuppliers.html.twig', [
             'controller_name' => 'SuppliersController',
         ]);
     }
