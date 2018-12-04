1c454812b29b2bfd3298d5fad74218ca1a907b40
tomasjur <tomasjkaunas@gmail.com>
2018-12-04 15:52:25 +0200

    Tiekejo DB lentele ir entity

---
+++ src/Entity/Supplier.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\SupplierRepository")
+ */
+class Supplier
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $company_code;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $address;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCompanyCode(): ?string
+    {
+        return $this->company_code;
+    }
+
+    public function setCompanyCode(string $company_code): self
+    {
+        $this->company_code = $company_code;
+
+        return $this;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getAddress(): ?string
+    {
+        return $this->address;
+    }
+
+    public function setAddress(string $address): self
+    {
+        $this->address = $address;
+
+        return $this;
+    }
+}
--- src/Migrations/Version20181201215010.php
+++ src/Migrations/Version20181201215010.php
@@ -20,6 +20,7 @@ final class Version20181201215010 extends AbstractMigration
         $this->addSql('ALTER TABLE car ADD CONSTRAINT FK_773DE69DA76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE visit ADD CONSTRAINT FK_437EE939A76ED395 FOREIGN KEY (user_id) REFERENCES user (id)');
         $this->addSql('ALTER TABLE visit ADD CONSTRAINT FK_437EE939C3C6F69F FOREIGN KEY (car_id) REFERENCES car (id)');
+        $this->addSql('CREATE TABLE supplier (id INT AUTO_INCREMENT NOT NULL, company_code VARCHAR(255) NOT NULL, name VARCHAR(255) NOT NULL, address VARCHAR(255) NOT NULL, PRIMARY KEY(id)) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci ENGINE = InnoDB');
     }
 
     public function down(Schema $schema) : void
@@ -30,5 +31,6 @@ final class Version20181201215010 extends AbstractMigration
         $this->addSql('ALTER TABLE visit DROP FOREIGN KEY FK_437EE939C3C6F69F');
         $this->addSql('DROP TABLE car');
         $this->addSql('DROP TABLE visit');
+        $this->addSql('DROP TABLE supplier');
     }
 }
